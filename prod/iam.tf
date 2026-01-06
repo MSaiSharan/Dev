a4b237efc33e70583eb009f1d923143adac6755a
Your Name <your@email.com>
2026-01-06 23:11:08 +0530

    update IAM policies

---
+++ prod/iam.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role" "vpc_flow_log" {
+    name = "vpc-flow-log-role"
+    
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+        {
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+            Service = "vpc-flow-logs.amazonaws.com"
+            }
+        }
+        ]
+    })
+  
+}
