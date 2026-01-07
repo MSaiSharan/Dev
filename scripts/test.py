78ac2ec13ad37240a190241ea1cfcf6b212956c8
Your Name <your@email.com>
2026-01-08 02:16:30 +0530

    update python code

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,53 @@
 import boto3
 
-ec2 = boto3.client('ec2')
+ec2 = boto3.client("ec2", region_name="us-east-1")
 
-vpcs = ec2.describe_vpcs()
+response = ec2.describe_vpcs(
+    Filters=[
+        {
+            "Name": "tag:Name",
+            "Values": ["dev-vpc"]
+        }
+    ]
+)
 
-print("VPC Information:")
-for vpc in vpcs['Vpcs']:
-    print(f"VPC ID: {vpc['VpcId']}  | CIDR: {vpc['CidrBlock']}")
+vpc_id = response["Vpcs"][0]["VpcId"]
+print("VPC ID:", vpc_id)
+
+subnets = ec2.describe_subnets(
+    Filters=[
+        {
+            "Name": "vpc-id",
+            "Values": [vpc_id]
+        }
+    ]
+)
+for subnet in subnets["Subnets"]:
+    print("Subnet ID:", subnet["SubnetId"])
+
+
+igws= ec2.describe_internet_gateways(
+    Filters=[
+        {
+            "Name": "attachment.vpc-id",
+            "Values": [vpc_id]
+        }
+    ]
+)
+for igw in igws["InternetGateways"]:
+    print("Internet Gateway ID:", igw["InternetGatewayId"])
+
+
+natgateways=ec2.describe_nat_gateways(
+    Filters=[
+        {
+            "Name": "vpc-id",
+            "Values": [vpc_id]
+        }
+    ]
+)
+for nat in natgateways["NatGateways"]:
+    print("NAT Gateway in VPC ID:", nat["NatGatewayId"])
+
+aws_account_id=boto3.client("sts").get_caller_identity().get("Account")
+print("AWS Account ID:", aws_account_id)
