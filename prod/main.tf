f4ad367eb8ad65a9224c3214c9b3ecd64cbc0053
Your Name <your@email.com>
2026-01-20 21:37:25 +0530

    Create a new instance

--- prod/main.tf
+++ prod/main.tf
@@ -83,3 +83,13 @@ resource "aws_cloudwatch_log_group" "dev_log_group" {
   retention_in_days = 7
 
 }
+
+resource "aws_instance" "dev_instance" {
+  ami                    = "ami-linux" # Example AMI ID
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.dev_security_group.id]
+  tags = {
+    Name = "dev-insgittance"
+  }
+}
